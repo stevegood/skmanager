66a0960c69c406ac1ae29da12373be100f4bcd86
Steve Good <steve@stevegood.org>
2013-10-10 13:24:49 -0500

    Added some dev data so i didn't have to manually enter it when I wanted to test things.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,3 +1,5 @@
+import org.stevegood.game.CharacterClass
+import org.stevegood.game.GameRole
 import org.stevegood.sec.Role
 import org.stevegood.sec.User
 import org.stevegood.sec.UserRole
@@ -9,6 +11,15 @@ class BootStrap {
             User admin = new User(username: 'admin', password: 'password', enabled: true).save(flush: true, insert: true)
             Role roleAdmin = new Role(authority: 'ROLE_ADMIN').save(insert: true, flush: true)
             UserRole.create(admin, roleAdmin, true)
+        }
+        development {
+            ['Rogue', 'Mage', 'Warrior', 'Cleric'].each {
+                new CharacterClass(name: it).save(insert: true, flush: true)
+            }
+
+            ['Tank', 'Healer', 'Support', 'DPS'].each {
+                new GameRole(name: it).save(insert: true, flush: true)
+            }
         }
     }
     def destroy = {
